504366bdb3c83b73697b865a73e366c3977ec545
Sanjay Goel <messagesgoel-blip@users.noreply.github.com>
2026-06-08 01:04:32 -0400

    Reduce browser export duplication

--- packages/chonky/src/index.ts
+++ packages/chonky/src/index.ts
@@ -7,8 +7,7 @@ export { FileToolbar } from './components/external/FileToolbar';
 export { FileList } from './components/file-list/FileList';
 export { FileContextMenu } from './components/external/FileContextMenu';
 export { FullFileBrowser } from './components/external/FullFileBrowser';
-export { BrowserShell } from './components/browser-shell/BrowserShell';
-export { BrowserDestinationPicker, BrowserFolderTree } from './components/tree/BrowserFolderTree';
+export * from './browser';
 export { FilePreviewer, defaultPreviewRenderers } from './components/preview/FilePreviewer';
 export { MediaLightbox } from './components/preview/MediaLightbox';
 export { PreviewFallback } from './components/preview/PreviewFallback';
@@ -36,76 +35,6 @@ export type FileViewConfig = import('./types/file-view.types').FileViewConfig;
 export type FileViewConfigGrid = import('./types/file-view.types').FileViewConfigGrid;
 export type FileViewConfigList = import('./types/file-view.types').FileViewConfigList;
 export { ThumbnailGenerator } from './types/thumbnails.types';
-export { createBrowserOpaqueId } from './types/browser-item.types';
-export type {
-    BrowserAction,
-    BrowserActionPlacement,
-    BrowserActionSelectionScope,
-    BrowserActionTone,
-    BrowserFileOperationKind,
-    BrowserFolderChainItem,
-    BrowserItem,
-    BrowserItemCapabilities,
-    BrowserItemFlags,
-    BrowserItemKind,
-    BrowserItemRef,
-    BrowserMetadataValue,
-    BrowserOpaqueId,
-    BrowserPreviewAsset,
-    BrowserPreviewAssetKind,
-    BrowserPreviewRenderer,
-    BrowserPreviewTrack,
-    BrowserPreviewTrackKind,
-    BrowserPreviewUnavailableReason,
-    BrowserSelection,
-    BrowserSourceDescriptor,
-    BrowserSourceKind,
-    PreviewDescriptor,
-} from './types/browser-item.types';
-export type {
-    BrowserShellActionEvent,
-    BrowserShellContextMenuRenderProps,
-    BrowserShellItemEvent,
-    BrowserShellNavigateEvent,
-    BrowserShellProps,
-    BrowserShellToolbarRenderProps,
-    BrowserSortDirection,
-    BrowserSortKey,
-    BrowserSortState,
-    BrowserViewMode,
-} from './types/browser-shell.types';
-export type {
-    BrowserFileOperationEvent,
-} from './types/browser-operation.types';
-export type {
-    BrowserDestinationPickerProps,
-    BrowserFolderChildrenLoader,
-    BrowserFolderExpansionEvent,
-    BrowserFolderSelectionEvent,
-    BrowserFolderTreeNode,
-    BrowserFolderTreeProps,
-} from './types/browser-tree.types';
-export type {
-    BrowserDropTargetKind,
-    BrowserTransferDropSnapshot,
-    BrowserTransferIntent,
-    BrowserTransferModifierState,
-    BrowserTransferOperation,
-    BrowserTransferSourceSnapshot,
-    BrowserTransferTarget,
-} from './types/browser-transfer.types';
-export {
-    BROWSER_FILE_OPERATION_ACTIONS,
-    createBrowserFileOperationEvent,
-    fileOperationForAction,
-    isBrowserActionEnabled,
-    isSelectionScopeSatisfied,
-    selectedItemsForOperation,
-} from './util/browser-file-operations';
-export {
-    createBrowserTransferIntent,
-    resolveBrowserTransferOperation,
-} from './util/browser-transfer';
 export type {
     AvailableBrowserPreviewAsset,
     FilePreviewerProps,
@@ -122,17 +51,6 @@ export type {
     PreviewShellProps,
     PreviewTileProps,
 } from './types/preview-shell.types';
-export {
-    browserActionFixtures,
-    browserFolderChainFixtures,
-    browserItemFixtures,
-    browserSelectionFixture,
-    browserSourceFixtures,
-} from './fixtures/browser-items';
-export {
-    browserFolderTreeFixtures,
-    lazyBrowserFolderChildrenFixtures,
-} from './fixtures/browser-tree';
 export {
     adaptBrowserFolderChainToFileArray,
     adaptBrowserItemToFileData,
